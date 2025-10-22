cebbb9c4f3be39c906b216c8cec6f74c08514344
Rut <rutpatel392@gmail.com>
2025-10-21 23:30:51 -0400

    changes to homepage

--- src/HomePage/HomePage.js
+++ src/HomePage/HomePage.js
@@ -4,18 +4,6 @@ import logo from '../assets/Rut_Logo.png'
 //import profile from '../assets/Untitled.svg'
 
 function HomePage() {
-  const [hovered, setHovered] = useState(null);
-
-  useEffect(() => {
-    let timeout;
-    if (hovered) {
-      timeout = setTimeout(() => {
-        setHovered(null);
-      }, 1000); // 1 second timeout
-    }
-    return () => clearTimeout(timeout);
-  }, [hovered]);
-
   return (
     <div className={styles.homepage}>
       <section className={styles.header}>
@@ -25,26 +13,6 @@ function HomePage() {
         <div className={styles.tabs}> Contact </div>
       </section>
       
-      <section className={styles.hero}>
-        <div className={styles.left}>
-          <div className={styles.jobtitle}>Designer</div>
-          <div className={styles.jobtitle4}>Product desiging specializing in<br></br> UI design and system design</div>
-        </div>
-        <div className={styles.right}>
-          <div className={styles.jobtitle2}>coder</div>
-          <div className={styles.jobtitle3}>Front-End developer who writes clean,<br></br> elegant and efficent code</div>
-        </div>
-      </section>
-      <section className={styles.projects}>
-        <div className={styles.left}>
-          <div className={styles.jobtitle}>Designer</div>
-          <div className={styles.jobtitle4}>Product desiging specializing in<br></br> UI design and system design</div>
-        </div>
-        <div className={styles.right}>
-          <div className={styles.jobtitle2}>coder</div>
-          <div className={styles.jobtitle3}>Front-End developer who writes clean,<br></br> elegant and efficent code</div>
-        </div>
-      </section>
     </div>
   )
 }
