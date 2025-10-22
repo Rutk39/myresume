c20e398b2546681e16cf5996e510daea02270ad1
Rut <rutpatel392@gmail.com>
2025-10-22 01:24:06 -0400

    Compelete the intro part of th main page.

--- src/HomePage/HomePage.js
+++ src/HomePage/HomePage.js
@@ -1,6 +1,9 @@
 import React from 'react'
 import styles from '../HomePage/HomePage.module.css'
 import logo from '../assets/Rut_Logo.png'
+import { motion } from 'framer-motion'
+
+//import profile from '../assets/rutpatel.png'
 //import profile from '../assets/Untitled.svg'
 
 function HomePage() {
@@ -12,6 +15,23 @@ function HomePage() {
         <div className={styles.tabs}> Projects </div>
         <div className={styles.tabs}> Contact </div>
       </section>
+      <section className={`${styles.content} ${styles.sectionWithBackground}`}>
+      <motion.div
+        initial={{ opacity: 0, y: 30 }}
+        animate={{ opacity: 1, y: 0 }}
+        transition={{ duration: 0.8 }}
+      className={styles.intro}>
+          <div className={styles.horizontalLine}>
+          </div>
+          <h1 className={styles.name}>I'm Rut, a <br></br>Full Stack Developer</h1>
+
+          <p className={styles.description}>passionate about building dynamic, responsive web apps. Explore my skills, projects, and journey in web development—and feel free to reach out.
+          </p>
+        </motion.div>
+          
+          <button className={styles.downArrow}> V </button>
+          
+        </section>
       
     </div>
   )
