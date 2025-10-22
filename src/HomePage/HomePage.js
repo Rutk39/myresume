f4bf24f76db0288b77700a91835c9617413714ea
Rut <rutpatel392@gmail.com>
2025-10-21 23:33:24 -0400

    change the homepage title to "Welcome to Our Website"

--- src/HomePage/HomePage.js
+++ src/HomePage/HomePage.js
@@ -1,4 +1,4 @@
-import React, { useState, useEffect } from 'react'
+import React from 'react'
 import styles from '../HomePage/HomePage.module.css'
 import logo from '../assets/Rut_Logo.png'
 //import profile from '../assets/Untitled.svg'
